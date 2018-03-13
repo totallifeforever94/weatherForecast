0c21d16bbddbb5d648895676fc2d955ffdaf589f
Taras Sava <sava.taras@gmail.com>
2018-03-13 14:25:06 +0000

    BrokenAPI

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(document).ready( function() {
+    $('#getWeather').click( function() {
+        
+       let city = $('#city').val();
+       let key = '0520485efb9e035d7d5f37715a9db23d';
+       
+       $.ajax({
+           url: 'https://api.openweathermap.org/data/2.5/weather',
+           dataType: 'json',
+           metho: 'GET',
+           data: {q: city, appid: key, untis: 'metric'},
+           
+           success: function(data) {
+               let wf = "";
+               $.each(data.weather, function(index, val) {
+                    wf += '<p><b>' + data.name + '</b><img src=' + val.icon + '.png></p>'+
+                    data.main.temp + '&deg;C' + ' | ' + val.main + ', ' + val.description;
+               });
+               $('#showWeather').html(wf);
+           }
+           
+        });
+    });
+});
